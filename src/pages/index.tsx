24d1b7e5e2dd73111ff69819853132ae73aab6e0
Saidinar <saidinarali@gmail.com>
2024-06-24 22:31:57 +0700

    feat: pre-commit husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import styles from '@/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const a ="test lagi"
+  console.log("test", a);
   return (
     <>
       <Head>
